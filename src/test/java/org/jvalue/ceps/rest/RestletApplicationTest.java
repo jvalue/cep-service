29d2831bb02aac11d0fce1222f14b8e4afae0e41
Philipp Eichhorn <philipp.eichhorn@bitdroid.de>
2014-07-13 13:11:04 +0200

    test: added RestletApplicationTest

---
+++ src/test/java/org/jvalue/ceps/rest/RestletApplicationTest.java
@@ -0,0 +1,21 @@
+package org.jvalue.ceps.rest;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+import org.restlet.routing.Router;
+
+
+public final class RestletApplicationTest {
+
+	@Test
+	public final void testInboundRoot() {
+		Router root = (Router) new RestletApplication().createInboundRoot();
+
+		assertNotNull(root.getDefaultRoute());
+		assertEquals(2, root.getRoutes().size());
+
+	}
+
+}
