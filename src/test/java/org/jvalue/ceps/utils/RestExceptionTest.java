3e1be2b4943a6d00a8767e18ca9db877f3447c0d
Philipp Eichhorn <philipp.eichhorn@bitdroid.de>
2014-07-13 13:29:46 +0200

    test: slightly more tests in utils

--- src/test/java/org/jvalue/ceps/utils/RestExceptionTest.java
+++ src/test/java/org/jvalue/ceps/utils/RestExceptionTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 import java.io.IOException;
+import java.net.HttpURLConnection;
 
 import org.junit.Test;
 
@@ -11,17 +12,32 @@ import org.junit.Test;
 public final class RestExceptionTest {
 
 	@Test
-	public void testGet() {
-
-		RestException re1 = new RestException(404);
-		assertEquals(404, re1.getCode());
-		assertNotNull(re1.getMessage());
-
+	public void testCauseError() {
 		IOException cause = new IOException("error");
 		RestException re2 = new RestException(cause);
 		assertEquals(cause, re2.getCause());
 		assertNotNull(re2.getMessage());
+	}
 
+
+	@Test
+	public void testCodeError() {
+		testCode(HttpURLConnection.HTTP_NOT_MODIFIED);
+		testCode(HttpURLConnection.HTTP_BAD_REQUEST);
+		testCode(HttpURLConnection.HTTP_UNAUTHORIZED);
+		testCode(HttpURLConnection.HTTP_FORBIDDEN);
+		testCode(HttpURLConnection.HTTP_NOT_FOUND);
+		testCode(HttpURLConnection.HTTP_BAD_GATEWAY);
+		testCode(HttpURLConnection.HTTP_BAD_METHOD);
+		testCode(HttpURLConnection.HTTP_GONE);
+		testCode(HttpURLConnection.HTTP_OK);
+	}
+
+
+	private void testCode(int code) {
+		RestException re = new RestException(code);
+		assertEquals(code, re.getCode());
+		assertNotNull(re.getMessage());
 	}
 
 
