923831e06c4c5b71cdb0d971459d49e2c0cb508e
Philipp Eichhorn <philipp.eichhorn@bitdroid.de>
2014-07-13 12:09:18 +0200

    rest: DefaultRestlet always returns 404

--- src/main/java/org/jvalue/ceps/rest/DefaultRestlet.java
+++ src/main/java/org/jvalue/ceps/rest/DefaultRestlet.java
@@ -3,7 +3,6 @@ package org.jvalue.ceps.rest;
 import org.restlet.Request;
 import org.restlet.Response;
 import org.restlet.data.MediaType;
-import org.restlet.data.Method;
 import org.restlet.data.Status;
 
 
@@ -12,14 +11,19 @@ final class DefaultRestlet extends BaseRestlet {
 
 	@Override
 	protected void doGet(Request request, Response response) {
-		response.setEntity("Nothing here for now. Maybe later?", MediaType.TEXT_PLAIN);
-		response.setStatus(Status.CLIENT_ERROR_NOT_FOUND, "Nothing here");
+		handle(response);
 	}
 
 
 	@Override
 	protected void doPost(Request request, Response response) {
-		onInvalidMethod(response, Method.POST);
+		handle(response);
+	}
+
+
+	private void handle(Response response) {
+		response.setEntity("Nothing here for now. Maybe later?", MediaType.TEXT_PLAIN);
+		response.setStatus(Status.CLIENT_ERROR_NOT_FOUND, "Nothing here");
 	}
 
 
