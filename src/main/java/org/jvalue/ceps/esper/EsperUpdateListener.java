0fec02f9254d7f6a995307c52638b971d4a76259
Philipp Eichhorn <philipp.eichhorn@bitdroid.de>
2014-07-23 14:34:29 +0200

    esper: EsperUpdateListener translates (most) esper classes to json

--- src/main/java/org/jvalue/ceps/esper/EsperUpdateListener.java
+++ src/main/java/org/jvalue/ceps/esper/EsperUpdateListener.java
@@ -1,12 +1,20 @@
 package org.jvalue.ceps.esper;
 
+import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
 
 import org.jvalue.ceps.utils.Assert;
+import org.jvalue.ceps.utils.Log;
 
 import com.espertech.esper.client.EventBean;
+import com.espertech.esper.client.EventType;
 import com.espertech.esper.client.UpdateListener;
+import com.espertech.esper.client.util.JSONEventRenderer;
+import com.espertech.esper.client.util.JSONRenderingOptions;
+import com.espertech.esper.event.util.JSONRendererImpl;
+import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -14,6 +22,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 final class EsperUpdateListener implements UpdateListener {
 
 	private static final ObjectMapper mapper = new ObjectMapper();
+	static {
+		mapper.setVisibility(PropertyAccessor.FIELD, Visibility.ANY);
+	}
+
 
 	private final JsonUpdateListener listener;
 	private final String eplStmtId;
@@ -37,7 +49,20 @@ final class EsperUpdateListener implements UpdateListener {
 		if (events == null) return jsonList;
 
 		for (EventBean event : events) {
-			jsonList.add(mapper.valueToTree(event.getUnderlying()));
+
+			EventType eventType = event.getEventType();
+			JSONEventRenderer renderer = new JSONRendererImpl(eventType, new JSONRenderingOptions());
+			String json = renderer.render("someEvent", event);
+			Log.info(json);
+
+			try {
+				JsonNode node = mapper.readTree(json);
+				jsonList.add(node);
+				// jsonList.add(mapper.valueToTree(event.getUnderlying()));
+			} catch (IOException ioe) {
+				throw new RuntimeException(ioe);
+			}
+
 		}
 		return jsonList;
 	}
