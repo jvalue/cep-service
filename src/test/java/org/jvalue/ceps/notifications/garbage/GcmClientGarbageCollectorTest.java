68ae192f534c328a9df50e91aac5bcbb118035c2
Philipp Eichhorn <philipp.eichhorn@bitdroid.de>
2014-12-30 22:33:09 +0100

    test: added GcmClientGarbageCollectorTest

---
+++ src/test/java/org/jvalue/ceps/notifications/garbage/GcmClientGarbageCollectorTest.java
@@ -0,0 +1,56 @@
+package org.jvalue.ceps.notifications.garbage;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.jvalue.ceps.notifications.clients.GcmClient;
+import org.jvalue.ceps.notifications.utils.GcmUtils;
+
+import java.util.Map;
+
+import mockit.Expectations;
+import mockit.Mocked;
+import mockit.Verifications;
+import mockit.integration.junit4.JMockit;
+
+
+@RunWith(JMockit.class)
+public final class GcmClientGarbageCollectorTest {
+
+	@Mocked private GcmUtils gcmUtils;
+	@Mocked private GcmUtils.GcmResult gcmResult;
+
+	@Test
+	public void testStatusCollect() {
+		testStatus(true);
+	}
+
+
+	@Test
+	public void testStatusRetain() {
+		testStatus(false);
+	}
+
+
+	@SuppressWarnings("unchecked")
+	private void testStatus(final boolean collect) {
+		new Expectations() {{
+			gcmUtils.sendMsg(anyString, (Map) any);
+			result = gcmResult;
+
+			gcmResult.isNotRegistered();
+			result = collect;
+		}};
+
+		final String deviceId = "someDeviceId";
+		GcmClient client = new GcmClient("someClientId", deviceId, "someEplStmt");
+		CollectionStatus status = new GcmClientGarbageCollector(gcmUtils).determineStatus(client);
+
+		if (collect) Assert.assertEquals(CollectionStatus.COLLECT, status);
+		else Assert.assertEquals(CollectionStatus.RETAIN, status);
+		new Verifications() {{
+			gcmUtils.sendMsg(deviceId, (Map) any);
+		}};
+	}
+
+}
