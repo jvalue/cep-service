b24e37ce53eb366e8c56bbcf80909444390e805c
Philipp Eichhorn <philipp.eichhorn@bitdroid.de>
2015-02-04 22:44:45 +0100

    rest: SourcesApi returns 409 if trying to register invalid source

--- src/main/java/org/jvalue/ceps/rest/SourcesApi.java
+++ src/main/java/org/jvalue/ceps/rest/SourcesApi.java
@@ -6,6 +6,7 @@ import com.google.inject.Inject;
 import org.jvalue.ceps.data.DataManager;
 import org.jvalue.ceps.data.OdsRegistration;
 import org.jvalue.common.rest.RestUtils;
+import org.jvalue.ods.api.sources.DataSourceApi;
 
 import java.util.List;
 
@@ -18,23 +19,33 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import retrofit.RetrofitError;
+
 @Path("/sources")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public final class SourcesApi {
 
 	private final DataManager dataManager;
+	private final DataSourceApi odsSourceApi;
 
 	@Inject
-	public SourcesApi(DataManager dataManager) {
+	public SourcesApi(DataManager dataManager, DataSourceApi odsSourceApi) {
 		this.dataManager = dataManager;
+		this.odsSourceApi = odsSourceApi;
 	}
 
 
 	@POST
 	@Path("/{sourceId}")
 	public OdsRegistration registerSource(@PathParam("sourceId") String sourceId) {
-		if (dataManager.isBeingMonitored(sourceId)) RestUtils.createJsonFormattedException("already being monitored", 409);
+		try {
+			// check that source is registered on ODS
+			odsSourceApi.get(sourceId);
+		} catch (RetrofitError re) {
+			throw RestUtils.createJsonFormattedException("source not registered on ODS", 409);
+		}
+		if (dataManager.isBeingMonitored(sourceId)) throw RestUtils.createJsonFormattedException("already being monitored", 409);
 		return dataManager.startMonitoring(sourceId);
 	}
 
@@ -42,7 +53,7 @@ public final class SourcesApi {
 	@GET
 	@Path("/{sourceId}")
 	public OdsRegistration getRegisteredSource(@PathParam("sourceId") String sourceId) {
-		if (!dataManager.isBeingMonitored(sourceId))  RestUtils.createNotFoundException();
+		if (!dataManager.isBeingMonitored(sourceId)) throw RestUtils.createNotFoundException();
 		return dataManager.get(sourceId);
 	}
 
@@ -56,7 +67,7 @@ public final class SourcesApi {
 	@DELETE
 	@Path("/{sourceId}")
 	public void unregisterSource(@PathParam("sourceId") String sourceId) {
-		if (dataManager.isBeingMonitored(sourceId)) RestUtils.createJsonFormattedException("already being monitored", 409);
+		if (dataManager.isBeingMonitored(sourceId)) throw RestUtils.createJsonFormattedException("already being monitored", 409);
 		dataManager.stopMonitoring(sourceId);
 	}
 
