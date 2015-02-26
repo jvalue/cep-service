b75e7b4681adde9e6859ce5ed492acd4d51d7809
Philipp Eichhorn <philipp.eichhorn@bitdroid.de>
2015-02-26 16:29:00 +0100

    models: fixed bug in ArgumentTypeDeserializer that lead to NPEs

--- models/src/main/java/org/jvalue/ceps/api/adapter/ArgumentTypeDeserializer.java
+++ models/src/main/java/org/jvalue/ceps/api/adapter/ArgumentTypeDeserializer.java
@@ -2,20 +2,17 @@ package org.jvalue.ceps.api.adapter;
 
 
 import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
-import com.fasterxml.jackson.databind.node.TextNode;
 
 import java.io.IOException;
 
 public final class ArgumentTypeDeserializer extends JsonDeserializer<ArgumentType> {
 
 	@Override
-	public ArgumentType deserialize(JsonParser parser, DeserializationContext context) throws IOException, JsonProcessingException {
-		TextNode node = parser.getCodec().readTree(parser);
+	public ArgumentType deserialize(JsonParser parser, DeserializationContext context) throws IOException {
 		try {
-			return ArgumentType.valueOf(node.asText());
+			return ArgumentType.valueOf(parser.getValueAsString());
 		} catch (IllegalArgumentException iae) {
 			throw new IOException(iae);
 		}
--- models/src/test/java/org/jvalue/ceps/api/adapter/ArgumentTypeTest.java
+++ models/src/test/java/org/jvalue/ceps/api/adapter/ArgumentTypeTest.java
@@ -8,10 +8,10 @@ import org.junit.Test;
 
 public final class ArgumentTypeTest {
 
+	private static final ObjectMapper mapper = new ObjectMapper();
+
 	@Test
 	public void testJson() throws Exception {
-		ObjectMapper mapper = new ObjectMapper();
-
 		Assert.assertEquals(ArgumentType.BOOLEAN, mapper.treeToValue(mapper.valueToTree(ArgumentType.BOOLEAN), ArgumentType.class));
 		Assert.assertEquals(ArgumentType.NUMBER, mapper.treeToValue(mapper.valueToTree(ArgumentType.NUMBER), ArgumentType.class));
 		Assert.assertEquals(ArgumentType.STRING, mapper.treeToValue(mapper.valueToTree(ArgumentType.STRING), ArgumentType.class));
